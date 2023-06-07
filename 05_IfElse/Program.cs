16b6efbfc854c8a1f1577252f647f8d52e43415a
TBolotova <tbolotova48@gmail.com>
2023-06-07 21:18:30 +0300

    Added conditional greetings

---
+++ 05_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Please Enter your name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "alex")
+{
+    Console.Write("Meow-meow-purr, " + username + " <3");
+}
+else 
+{
+    Console.WriteLine("Hi, " + username);
+}
