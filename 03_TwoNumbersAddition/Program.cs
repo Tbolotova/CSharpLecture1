486f74db0a8edbb9382852c1a6fe8edababd7da6
TBolotova <tbolotova48@gmail.com>
2023-06-07 21:06:51 +0300

    Randomized numbers in 03

--- 03_TwoNumbersAddition/Program.cs
+++ 03_TwoNumbersAddition/Program.cs
@@ -1,4 +1,6 @@
-﻿int firstNumber = 3;
-int secondNumber = 5;
+﻿int firstNumber = new Random().Next(1, 10); //1 <=random number < 10
+Console.WriteLine(firstNumber);
+int secondNumber = new Random().Next(1, 10);
+Console.WriteLine(secondNumber);
 int result = firstNumber + secondNumber;
 Console.WriteLine(result);
